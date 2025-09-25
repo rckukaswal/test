4281118ab8b622bcb32c664ff9c9dd08f7d37f0b
rckukaswal <ramchandrakukaswal@gmail.com>
2025-09-25 13:13:30 +0530

    Create HelloWorld.java

---
+++ programs/HelloWorld.java
@@ -0,0 +1,8 @@
+package programs;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is my first Java program.");
+    }
+}
